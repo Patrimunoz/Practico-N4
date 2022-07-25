64428ca49b91b70c7c520ebc9730b5e76ac8fc96
Patrimunoz <95888440+Patrimunoz@users.noreply.github.com>
2022-07-25 11:06:49 -0300

    Update Ejercicios.js

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,14 +1,19 @@
 function Obtener(obj) {
     //Obtener y Recorrer las Llaves de un Objeto
     //Aqui tu codigo
-    conso
+   var arr = ['hola', 'chau', 'adios'];
+console.log(Object.keys(arr));
+   
 
 }
 
 function Fecha(num) {
     //Escriba una función de JavaScript para obtener el día de una fecha en particular.
     //Aqui tu codigo
+var Anniversary22 = new Date('October 8, 2022 12:30');
+var weekday = Anniversary22.getDay();
 
+console.log(weekday); 
 }
 
 function verificarPassword(usuario, password) {
@@ -16,9 +21,11 @@ function verificarPassword(usuario, password) {
     // Devuelve "true" si coinciden
     // De lo contrario, devuelve "false"
     // // Tu código:
-    console.log("Hola")
-    var a=40
-
+   
+if(password === usuario.password){
+    return true;
+}
+    return false;
 }
 
 function crearGato(nombre, edad) {
@@ -27,6 +34,18 @@ function crearGato(nombre, edad) {
     // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
     // Devuelve el objeto
     // Tu código:
+    
+     var gato = {
+    nombre: nombre,
+    edad: edad,
+    meow: function () {
+      return "Meow!";
+    }
+  };
+  return gato;
+}
+var miGato = crearGato('Benji', 2);
+console.log(miGato); 
 
 }
 function agregarMetodoCalculoDescuento(producto) {
@@ -39,9 +58,22 @@ function agregarMetodoCalculoDescuento(producto) {
     // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
     // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
     // Tu código:
+  var cantidad,precio,desc,compra,pagar;
+    
+    precio = parseFloat(prompt("Ingresar precio"));
+    cantidad = parseFloat(prompt("Ingresar cantidad"));
+    
+    compra = precio*cantidad;
+    desc = compra*.25;
+    pagar = compra-desc;
+   
+    document.write("El descuento es: "+desc);
+    document.write("El total a pagar es: "+pagar);
+ 
+    
 
 }
-function ordenar(array) {
+function ordenarArray
     // A partir del array pasado por parametros, ordenarlo (ascendente o descendente) de la forma que tu quieras
     // Mostrar array ordenado.
     // Tu código:
